63b5ed6b261cc22cc9297d5aa38188c49feb4b56
Zenoviy Fil <fil.zenoviy@gmail.com>
2023-11-20 09:27:20 +0200

    task-4 create

---
+++ js/task-4.js
@@ -0,0 +1,48 @@
+const getTotalBalanceByGender = (users, gender) => {
+    return users.reduce((total, user) => {
+        return total + user.balance
+    }, 0)
+};
+
+
+const allUsers = [
+    {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
